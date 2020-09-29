7bb5d3b7cfa2fd28dec0cf33beb14c417c77928e
VaahnKat <45543460+VaahnKat@users.noreply.github.com>
2020-09-29 09:32:58 +0200

    code created
    Given a range of first 10 numbers, Iterate from start number to the end number and print the sum of the current number and previous number

---
+++ Sumofprev.py
@@ -0,0 +1,7 @@
+y = range(10)
+for x in y:
+    c=x+x-1
+    if x == 0:
+        print("the Answer is as follows")
+    else:
+        print ("the sum of Current Number and ",x,"the previous number ",x-1,"is", c)
