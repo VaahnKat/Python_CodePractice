da83641e609a0e99880ca5a8b5cce9cf4fd1a7cd
VaahnKat <45543460+VaahnKat@users.noreply.github.com>
2020-09-29 21:23:26 +0200

    Create even_char.py
    Given a string, display only those characters which are present at an even index number.

---
+++ even_char.py
@@ -0,0 +1,13 @@
+def evenchar(x):
+    print (x)
+    y=len(x)
+    print(y)
+    for i in range(0,y-1,2):
+        print("the even chars of the given string ",x,"is as follows",x[i])
+inputstr= "nOvEmBeR"
+print("The given character is ")
+evenchar(inputstr)
+    
+    
+
+    
