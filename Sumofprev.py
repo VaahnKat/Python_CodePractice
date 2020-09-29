46a6beb542e3c88bd63cf5c401142c496356f6ba
VaahnKat <45543460+VaahnKat@users.noreply.github.com>
2020-09-29 09:39:23 +0200

    updated

--- Sumofprev.py
+++ Sumofprev.py
@@ -1,7 +1,10 @@
-y = range(10)
-for x in y:
-    c=x+x-1
-    if x == 0:
-        print("the Answer is as follows")
-    else:
-        print ("the sum of Current Number and ",x,"the previous number ",x-1,"is", c)
+def sumprev(z):
+    y = range(z)
+    for x in y:
+        c=x+x-1
+        if x == 0:
+            print("the Answer is as follows")
+        else:
+            print ("the sum of Current Number and ",x,"the previous number ",x-1,"is", c)
+
+sumprev(5)
